fc817c156f0d70035c87b4df3030a5e74ad1937f
Nikita Kozlov <66692096+Mip182@users.noreply.github.com>
2022-12-24 00:55:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,11 +13,25 @@ dp = Dispatcher(bot)
 async def process_start_command(message: types.Message):
     await message.reply("Hi!\nWrite me smthing!")
 
-
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.reply("Write me smthing and I will send you this text!")
-    
+
+@dp.message_handler(commands=['voice'])
+async def process_help_command(message: types.Message):
+    await message.reply("Voice!!")
+
+@dp.message_handler(commands=['group'])
+async def process_help_command(message: types.Message):
+    await message.reply("Group!!")
+
+@dp.message_handler(commands=['note'])
+async def process_help_command(message: types.Message):
+    await message.reply("Note!!")
+
+@dp.message_handler(commands=['file'])
+async def process_help_command(message: types.Message):
+    await message.reply("File!!")    
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
